d1515b880c6eb4da19a1f9a1c107335660b9919a
mdev <30726744+m-devs@users.noreply.github.com>
2020-02-05 23:19:36 +0100

    Logger import fix, name refactor
    Signed-off-by: m-devs <markodevelopment@gmail.com>

--- src/main/java/mdev/OrderProcessingSpring/OPSpringApp.java
+++ src/main/java/mdev/OrderProcessingSpring/OPSpringApp.java
@@ -1,5 +1,6 @@
 package mdev.OrderProcessingSpring;
 
+import ch.qos.logback.classic.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -7,22 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.logging.Logger;
-
 /**
  * @author markodevelopment (Mihálovics Márkó)
  * @// TODO: 2/5/20 Create the Spring version of OrderProcessing..
  */
 @SpringBootApplication
-public class OrderProcessingSpringApplication implements CommandLineRunner{
+public class OPSpringApp implements CommandLineRunner{
 
-	private static final Logger log = (Logger) LoggerFactory.getLogger(OrderProcessingSpringApplication.class);
+	private static final Logger log = (Logger) LoggerFactory.getLogger(OPSpringApp.class);
 
 	@Autowired
 	JdbcTemplate jdbc;
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderProcessingSpringApplication.class, args);
+		SpringApplication.run(OPSpringApp.class, args);
 	}
 
 	@Override
